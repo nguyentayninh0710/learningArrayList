71d680c20967c6f42c7a21889b2c9b3f46397c92
Huynh Ky Khoi Nguyen <huynhkykhoinguyen0710@gmail.com>
2025-08-05 21:13:24 +0700

    asign_excercise_1

---
+++ src/demoCode/Exercise1.java
@@ -0,0 +1,17 @@
+package demoCode;
+//Exercise 1: Create a program to manage a list of integers
+//Input n integers from the user
+//
+//Display the list
+//
+//Remove all even numbers
+//
+//Display the updated list
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
